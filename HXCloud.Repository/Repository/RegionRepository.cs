9cf6300bc77dd2eeb6b7ccd764f8afa0cbd139ea
hx <xzc02106430@163.com>
2020-07-24 17:41:45 +0800

    modify region

--- HXCloud.Repository/Repository/RegionRepository.cs
+++ HXCloud.Repository/Repository/RegionRepository.cs
@@ -1,11 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
+using System.Threading.Tasks;
 using HXCloud.Model;
 
 namespace HXCloud.Repository
 {
     public class RegionRepository : BaseRepository<RegionModel>, IRegionRepository
     {
+        //添加时更新父节点的删除节点
+        public async Task AddAsync(RegionModel entity,RegionModel parent)
+        {
+
+        }
+
+        //删除时更新父节点的删除节点
+        public  async Task RemoveAsync(RegionModel entity,RegionModel parent)
+        {
+
+        }
     }
 }
