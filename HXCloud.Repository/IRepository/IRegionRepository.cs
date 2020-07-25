0418bbe9e2c4904992ffa7df8491b3aa39de3a4f
qkxyk <xzc02106430@163.com>
2020-07-25 18:29:48 +0800

    addregion

--- HXCloud.Repository/IRepository/IRegionRepository.cs
+++ HXCloud.Repository/IRepository/IRegionRepository.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using HXCloud.Model;
 
 namespace HXCloud.Repository
 {
     public interface IRegionRepository:IBaseRepository<RegionModel>
     {
+        Task AddAsync(RegionModel entity, RegionModel parent);
+        Task RemoveAsync(RegionModel entity, RegionModel parent);
     }
 }
--- HXCloud.Repository/Repository/RegionRepository.cs
+++ HXCloud.Repository/Repository/RegionRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using HXCloud.Model;
 
 namespace HXCloud.Repository
@@ -10,15 +11,26 @@ namespace HXCloud.Repository
     public class RegionRepository : BaseRepository<RegionModel>, IRegionRepository
     {
         //添加时更新父节点的删除节点
-        public async Task AddAsync(RegionModel entity,RegionModel parent)
+        public async Task AddAsync(RegionModel entity, RegionModel parent)
         {
-
+            //_db.Set<RegionModel>..Regions.Add(entity);
+            _db.Regions.Add(entity);
+            if (parent != null)
+            {
+                _db.Entry<RegionModel>(parent).State = EntityState.Modified;//更新父节点
+            }
+            await _db.SaveChangesAsync();
         }
 
         //删除时更新父节点的删除节点
-        public  async Task RemoveAsync(RegionModel entity,RegionModel parent)
+        public async Task RemoveAsync(RegionModel entity, RegionModel parent)
         {
-
+            _db.Regions.Remove(entity);
+            if (parent != null)
+            {
+                _db.Entry(parent).State = EntityState.Modified;
+            }
+            await _db.SaveChangesAsync();
         }
     }
 }
